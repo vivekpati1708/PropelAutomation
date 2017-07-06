e2c0b29c899f1d7ec438a4b315101aace32e9df8
maraka <maraka@MARAKA4.asiapacific.hpqcorp.net>
2017-07-06 15:10:54 +0530

    Updated Loginpage

--- src/propel/pageclasses/com/LoginPage.java
+++ src/propel/pageclasses/com/LoginPage.java
@@ -4,15 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 public class LoginPage {
   
 	WebDriver driver;
 	
-	LoginPage(WebDriver driver)
+	 public LoginPage(WebDriver driver)
 	{
 		this.driver=driver;
+		PageFactory.initElements(driver,this); 
 	}
 	
 	@FindBy(id="Name") 
@@ -29,7 +31,7 @@ public class LoginPage {
 	
 	public void enterLoginInfo()
 	{
-		tbusername.sendKeys("vivek.pati@dxc.com");
+		tbusername.sendKeys("vivek.pati@hpe.com");
 		tbpassword.sendKeys("anyone");
 		btnLogin.click();
 		
