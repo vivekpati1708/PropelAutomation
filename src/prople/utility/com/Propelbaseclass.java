4dc258a80812145dc805daaa7d23c2756b2f830e
maraka <maraka@MARAKA4.asiapacific.hpqcorp.net>
2017-07-06 12:16:04 +0530

    Browser start method updated

--- src/prople/utility/com/Propelbaseclass.java
+++ src/prople/utility/com/Propelbaseclass.java
@@ -3,38 +3,33 @@ package prople.utility.com;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
-public class Propelbaseclass {
+public class Propelbaseclass 
+{
 	WebDriver driver;
   
 	public void startBrowser(String browserName,String url)
 	
 	{
-		switch (browserName.toUpperCase().trim()) {
+		switch (browserName.toUpperCase().trim()) 
+		{
 		case "FIREFOX":
 			driver=new FirefoxDriver();
 			break;
 		case "CHROME":
-			driver=new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\maraka\\workspace\\PropelAutomation\\Data\\Driverschromedriver.exe");
+			driver = new ChromeDriver();
 			break;	
 		case "IE":
-			driver=new InternetExplorer();
+			System.setProperty("webdriver.ie.driver","C:\\Users\\maraka\\workspace\\PropelAutomation\\Data\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 			break;	
 		default:
 			break;
 		}
-		if (browserName.equalsIgnoreCase("Firefox")) {
-			
-			driver=new FirefoxDriver();
-			
-		}
 		
-		else if (browserName.equalsIgnoreCase("chromer")) 
-		{
-			driver=new FirefoxDriver();
-			
-		}
 		
 	}
 	
