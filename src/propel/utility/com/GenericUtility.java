8aec5f9577fdd9084693d5b4b98f0609ecd03b5b
vivekpati1708 <pativivek1708@gmailc.com>
2017-07-11 19:58:02 +0530

    Data file and excel method

--- src/propel/utility/com/GenericUtility.java
+++ src/propel/utility/com/GenericUtility.java
@@ -3,7 +3,13 @@ package propel.utility.com;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -58,7 +64,48 @@ public class GenericUtility {
 		}
 	return ele;
 	}
-	 
+	 /**
+	  * Excel method the read the data from the excel. 
+	  * 
+	  */
+	public void GetData()
+	{
+		File xlsSrc = new File("..\\PropelAutomation\\src\\propel\\data\file\\PropelData.xlsx");
+		System.out.println("Inside0");
+		try
+		{
+			System.out.println("Inside1");
+		FileInputStream xlxFis = new FileInputStream(xlsSrc);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(xlxFis);
+
+		//For xlx file we will be using the class. 
+		//HSSFWorkbook wb1 = new HSSFWorkbook(xlxFis);
+
+		XSSFSheet loginData =wb.getSheet("LoginData"); // To get sheet as per the sheet name. 
+		//wb.getSheetAt(0);// Fetch you the 1st sheet as the index start from zero. 
+		
+		String userName = loginData.getRow(1).getCell(0).getStringCellValue();
+		String passworkd = loginData.getRow(1).getCell(1).getStringCellValue();
+		
+		System.out.println("Username="+userName);
+		System.out.println("Pwd="+passworkd);
+		wb.close();
+		
+		}
+		catch(IOException e)
+		{
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		GenericUtility obj = new GenericUtility();
+		System.out.println("Test");
+		obj.GetData();
+	}
 }
 
 
